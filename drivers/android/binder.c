4425cd713d3d0358db22fa8757742c2496fe493c
Stephen Smalley <sds@tycho.nsa.gov>
2012-11-05 08:15:34 -0500

    3Add security hooks to binder and implement the hooks for SELinux.
    Add security hooks to the binder and implement the hooks for SELinux.
    The security hooks enable security modules such as SELinux to implement
    controls over binder IPC.  The security hooks include support for
    controlling what process can become the binder context manager
    (binder_set_context_mgr), controlling the ability of a process
    to invoke a binder transaction/IPC to another process (binder_transaction),
    controlling the ability a process to transfer a binder reference to
    another process (binder_transfer_binder), and controlling the ability
    of a process to transfer an open file to another process (binder_transfer_file).
    
    This support is used by SE Android, http://selinuxproject.org/page/SEAndroid.
    
    Change-Id: I34266b66320b6a3df9ac01833d7f94daf742920e
    Signed-off-by: Stephen Smalley <sds@tycho.nsa.gov>

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -38,6 +38,8 @@
 #include <linux/slab.h>
 #include <linux/pid_namespace.h>
 #include <linux/ratelimit.h>
+#include <linux/security.h>
+>>>>>>> e221554046e5... Add security hooks to binder and implement the hooks for SELinux.
 
 #ifdef CONFIG_ANDROID_BINDER_IPC_32BIT
 #define BINDER_IPC_32BIT 1
@@ -1452,6 +1454,10 @@ static void binder_transaction(struct binder_proc *proc,
 			return_error = BR_DEAD_REPLY;
 			goto err_dead_binder;
 		}
+		if (security_binder_transaction(proc->tsk, target_proc->tsk) < 0) {
+			return_error = BR_FAILED_REPLY;
+			goto err_invalid_target_handle;
+		}
 		if (!(tr->flags & TF_ONE_WAY) && thread->transaction_stack) {
 			struct binder_transaction *tmp;
 			tmp = thread->transaction_stack;
@@ -1609,6 +1615,10 @@ static void binder_transaction(struct binder_proc *proc,
                 return_error = BR_FAILED_REPLY;
 				goto err_binder_get_ref_for_node_failed;
 			}
+			if (security_binder_transfer_binder(proc->tsk, target_proc->tsk)) {
+				return_error = BR_FAILED_REPLY;
+				goto err_binder_get_ref_for_node_failed;
+			}
 			ref = binder_get_ref_for_node(target_proc, node);
 			if (ref == NULL) {
 				return_error = BR_FAILED_REPLY;
@@ -1642,6 +1652,10 @@ static void binder_transaction(struct binder_proc *proc,
 				binder_user_error("%d:%d got transaction with invalid handle, %d\n",
 						proc->pid,
 						thread->pid, fp->handle);
+				return_error = BR_FAILED_REPLY;
+				goto err_binder_get_ref_failed;
+			}
+			if (security_binder_transfer_binder(proc->tsk, target_proc->tsk)) {
 				return_error = BR_FAILED_REPLY;
 				goto err_binder_get_ref_failed;
 			}
@@ -1707,6 +1721,11 @@ static void binder_transaction(struct binder_proc *proc,
 					proc->pid, thread->pid, fp->fd);
 				return_error = BR_FAILED_REPLY;
 				goto err_fget_failed;
+			}
+			if (security_binder_transfer_file(proc->tsk, target_proc->tsk, file) < 0) {
+				fput(file);
+				return_error = BR_FAILED_REPLY;
+				goto err_get_unused_fd_failed;
 			}
 			target_fd = task_get_unused_fd_flags(target_proc, O_CLOEXEC);
 			if (target_fd < 0) {
@@ -2763,42 +2782,44 @@ static int binder_ioctl_write_read(struct file *filp,
         goto out;
     }
 out:
-    return ret;
+	return ret;
 }
 
-static int binder_ioctl_set_ctx_mgr(struct file *filp, struct binder_thread
-*thread)
+static int binder_ioctl_set_ctx_mgr(struct file *filp)
 {
-    int ret = 0;
-    struct binder_proc *proc = filp->private_data;
-    kuid_t curr_euid = current_euid();
+	int ret = 0;
+	struct binder_proc *proc = filp->private_data;
+	kuid_t curr_euid = current_euid();
 
-    if (binder_context_mgr_node != NULL) {
-        pr_err("BINDER_SET_CONTEXT_MGR already set\n");
-        ret = -EBUSY;
-        goto out;
-    }
-    if (uid_valid(binder_context_mgr_uid)) {
-        if (!uid_eq(binder_context_mgr_uid, curr_euid)) {
-            pr_err("BINDER_SET_CONTEXT_MGR bad uid %d != %d\n",
-                    from_kuid(&init_user_ns, curr_euid),
-                    from_kuid(&init_user_ns,
-                    binder_context_mgr_uid));
-            ret = -EPERM;
-            goto out;
-        }
-    } else {
-        binder_context_mgr_uid = curr_euid;
-    }
-    binder_context_mgr_node = binder_new_node(proc, 0, 0);
-    if (binder_context_mgr_node == NULL) {
-        ret = -ENOMEM;
-        goto out;
-    }
-    binder_context_mgr_node->local_weak_refs++;
-    binder_context_mgr_node->local_strong_refs++;
-    binder_context_mgr_node->has_strong_ref = 1;
-    binder_context_mgr_node->has_weak_ref = 1;
+	if (binder_context_mgr_node != NULL) {
+		pr_err("BINDER_SET_CONTEXT_MGR already set\n");
+		ret = -EBUSY;
+		goto out;
+	}
+	ret = security_binder_set_context_mgr(proc->tsk);
+	if (ret < 0)
+		goto out;
+	if (uid_valid(binder_context_mgr_uid)) {
+		if (!uid_eq(binder_context_mgr_uid, curr_euid)) {
+			pr_err("BINDER_SET_CONTEXT_MGR bad uid %d != %d\n",
+			       from_kuid(&init_user_ns, curr_euid),
+			       from_kuid(&init_user_ns,
+					binder_context_mgr_uid));
+			ret = -EPERM;
+			goto out;
+		}
+	} else {
+		binder_context_mgr_uid = curr_euid;
+	}
+	binder_context_mgr_node = binder_new_node(proc, 0, 0);
+	if (binder_context_mgr_node == NULL) {
+		ret = -ENOMEM;
+		goto out;
+	}
+	binder_context_mgr_node->local_weak_refs++;
+	binder_context_mgr_node->local_strong_refs++;
+	binder_context_mgr_node->has_strong_ref = 1;
+	binder_context_mgr_node->has_weak_ref = 1;
 out:
     return ret;
 }
